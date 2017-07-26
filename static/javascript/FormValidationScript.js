d15a848e6c4b69b0ca8d0dfd183eff2e006a377d
Prateek90 <pc1899@nyu.edu>
2017-07-26 13:09:10 -0400

    Sixth Commit

--- static/javascript/FormValidationScript.js
+++ static/javascript/FormValidationScript.js
@@ -9,19 +9,19 @@ function inputBlur(i){
 function validateForm() {
     var x = document.getElementById("users");
     var y = document.getElementById("reviewCount");
-    if (x.value=="") {
+    if (x.value == "") {
         alert("Users cannot be empty");
         return false;
     }else if(y.value == ""){
         alert("Reviews per video cannot be empty")
         return false;
-    }else if (x.value <=0) {
+    }else if (parseInt(x.value,10) <= 0) {
         alert("Users should be greater than zero");
         return false;
-    }else if(y.value <= 0){
+    }else if(parseInt(y.value,10) < 0){
         alert("Reviews cannot be negative")
         return false;
-    }else if(x.value <= y.value){
+    }else if(parseInt(x.value,10) <= parseInt(y.value,10)){
         alert("Users should be greater than reviews per video")
         return false;
     }
